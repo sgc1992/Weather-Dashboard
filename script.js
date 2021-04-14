b53d3d80bdc6e50e0753e3f457a6c1e45d93d50d
Sandeep GC <sandeep_gc@hotmail.com>
2021-04-14 19:33:31 +1000

    added API to the function

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 var locationInput = document.querySelector("#search");
 var searchButton = document.querySelector("#submit");
 var locationSpan = document.querySelector("#location-span");
+var submitButton = document.querySelector("#submit")
+
+submitButton.addEventListener('click', function (name) {
+var locationInput = document.querySelector("#locationInput")
+console.log(locationInput.value)
+
+  fetch('https://api.openweathermap.org/data/2.5/weather?q='+locationInput+'&appid='+'07d0c65f5c20674ff54bcddb4b9e892f')
+    .then(response => response.json())
+    .then(data => {
+     console.log(data) 
+
+    })
+  })

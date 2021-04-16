d5a34f9f943bfac7064f5547827b3c089d752815
Sandeep GC <sandeep_gc@hotmail.com>
2021-04-17 08:08:25 +1000

    added label for temperature, wind and humidity

--- script.js
+++ script.js
@@ -3,17 +3,22 @@ var locationSpan = document.querySelector("#location-span");
 var submitButton = document.querySelector("#submit")
 
 submitButton.addEventListener('click', function (name) {
-var locationInput = document.querySelector("#locationInput")
-console.log(locationInput.value)
+  var locationInput = document.querySelector("#locationInput")
+  console.log(locationInput.value)
 
-  fetch('http://api.openweathermap.org/data/2.5/weather?q='+locationInput.value+'&appid='+'07d0c65f5c20674ff54bcddb4b9e892f')
+  fetch('http://api.openweathermap.org/data/2.5/weather?q=' + locationInput.value + '&appid=' + '07d0c65f5c20674ff54bcddb4b9e892f')
     .then(response => response.json())
     .then(data => {
-     console.log(data) 
-     document.getElementById("current-location").textContent = data.name;
-     document.getElementById("current-temp").textContent = data.main.temp;
-     document.getElementById("wind").textContent = data.wind.speed;
-     document.getElementById("humidity").textContent = data.main.humidity;
-     })
+      console.log(data)
+      document.getElementById("current-location").textContent = data.name;
+      document.getElementById("current-temp").textContent = data.main.temp;
+      document.getElementById("wind").textContent = data.wind.speed;
+      document.getElementById("humidity").textContent = data.main.humidity;
+    })
+
+  // fetch('http://api.openweathermap.org/data/2.5/forecast?q=' + locationInput.value + '&appid=' + '07d0c65f5c20674ff54bcddb4b9e892f')
+  //   .then(response => response.json())
+  //   .then(data => { }
+      
 
   })

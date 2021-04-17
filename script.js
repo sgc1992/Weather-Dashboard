1c130b19f4454b02445547c55e8133d0c494ee60
Sandeep GC <sandeep_gc@hotmail.com>
2021-04-18 06:42:23 +1000

    no changes

--- script.js
+++ script.js
@@ -6,14 +6,16 @@ submitButton.addEventListener('click', function (name) {
   var locationInput = document.querySelector("#locationInput")
   console.log(locationInput.value)
 
-  fetch('http://api.openweathermap.org/data/2.5/weather?q=' + locationInput.value + '&appid=' + '07d0c65f5c20674ff54bcddb4b9e892f')
+  fetch('http://api.openweathermap.org/data/2.5/weather?q=' + locationInput.value +  '&appid=' + '07d0c65f5c20674ff54bcddb4b9e892f')
     .then(response => response.json())
     .then(data => {
       console.log(data)
+      document.getElementById("weather-icon").textContent = data.weather.icon;
       document.getElementById("current-location").textContent = data.name;
       document.getElementById("current-temp").textContent = data.main.temp;
       document.getElementById("wind").textContent = data.wind.speed;
       document.getElementById("humidity").textContent = data.main.humidity;
+      // document.getElementById("uv-index").textContent = data.main.humidity;
     })
   fetch('http://api.openweathermap.org/data/2.5/forecast?q=' + locationInput.value + '&appid=' + '07d0c65f5c20674ff54bcddb4b9e892f')
     .then(response => response.json())

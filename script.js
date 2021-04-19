41ce072560d03e5207ff096b0f55c9dd5720d387
Sandeep GC <sandeep_gc@hotmail.com>
2021-04-20 06:48:37 +1000

    adding local storage

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ submitButton.addEventListener('click', function (name) {
   var locationInput = document.querySelector("#locationInput")
   console.log(locationInput.value)
 
-  fetch('https://api.openweathermap.org/data/2.5/weather?q=' + locationInput.value + '&appid=' + '07d0c65f5c20674ff54bcddb4b9e892f'+'&units=metric')
+  fetch('https://api.openweathermap.org/data/2.5/weather?q=' + locationInput.value + '&appid=' + '07d0c65f5c20674ff54bcddb4b9e892f' + '&units=metric')
     .then(response => response.json())
     .then(data => {
       console.log(data)
@@ -19,11 +19,22 @@ submitButton.addEventListener('click', function (name) {
       document.getElementById("wind").textContent = data.wind.speed;
       document.getElementById("humidity").textContent = data.main.humidity;
       // document.getElementById("uv-index").textContent = data.main.humidity;
+      window.localStorage.setItem('current-location', 'data.name');
+      window.localStorage.setItem('current-temp', 'data.main.temp');
+      window.localStorage.setItem('current-location', 'data.wind.speed');
+      window.localStorage.setItem('current-location', 'data.main.humidity');
     })
+
+
   fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + locationInput.value + '&appid=' + '07d0c65f5c20674ff54bcddb4b9e892f')
     .then(response => response.json())
     .then(data => {
       console.log(data)
+      for (var i = 0; i < data.list.length; i++) {
+        console.log(data.list[i])
+        var time = moment(data.list[i].dt)
+        console.log(time)
+      }
 
     })
 })

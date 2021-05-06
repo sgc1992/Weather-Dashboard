6c13ac21ac840975fecb9eff78705e1b8c4058d0
Sandeep GC <sandeep_gc@hotmail.com>
2021-05-07 08:44:48 +1000

    deleted card for 5 days forecast in index

--- script.js
+++ script.js
@@ -33,28 +33,10 @@ submitButton.addEventListener('click', function (name) {
       console.log(data);
 
       // data.daily has the 7 day forecast
-      var icon = "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
-      var newImage = document.createElement("img") //make an img tag.
-      newImage.setAttribute('src', icon)
-      document.getElementById("icon").append(newImage);
-      document.getElementById("current-location").textContent = data.name;
-      document.getElementById("current-temp").textContent = data.main.temp;
-      document.getElementById("wind").textContent = data.wind.speed;
-      document.getElementById("humidity").textContent = data.main.humidity;
+      
       
     });
     
   
 })
 
-// fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + locationInput.value + '&appid=' + 'accd5d59f380f6347ac44664ded3ecef')
-//     .then(response => response.json())
-//     .then(data => {
-//       console.log('5 day')
-//       console.log(data)
-//       for (let i = 0; i < data.list.length; i = i + 9) {
-//         // this gets the forecast every 24 hours for 5 days
-//         // use this to generate the 5 day forecast elements in the html
-//         console.log(data.list[i])
-//       }
-//     });

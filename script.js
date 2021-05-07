1eec7508f3e1b309a24b464c086259032877ca6d
Sandeep GC <sandeep_gc@hotmail.com>
2021-05-08 07:50:04 +1000

    added comment

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
+//created a variable and getting first element with query selector
 var locationInput = document.querySelector("#search");
 var locationSpan = document.querySelector("#location-span");
+//created a variable and getting element by id from the index html
 var submitButton = document.getElementById("submit")
 var displayResults = document.getElementById("card")
 
+//created a function for click wth addEvent listener
 submitButton.addEventListener('click', function (name) {
   var locationInput = document.querySelector("#locationInput")
   console.log(locationInput.value)
 
+//fetching the data from the open weather api with api Key with units of measurement as metric
   fetch('https://api.openweathermap.org/data/2.5/weather?q=' + locationInput.value + '&appid=' + '07d0c65f5c20674ff54bcddb4b9e892f' + '&units=metric')
     .then(response => response.json())
     .then(data => {
+//Using template Literals with placeholder
       const html = `
       <div>Current-Weather</div>
       <div class="card text-white bg-dark mb-3" style="max-width: 10rem;">
@@ -23,7 +28,7 @@ submitButton.addEventListener('click', function (name) {
       <label>Humidity</label>
       <p id="humidity">${data.main.humidity}</p>
       </div>`
-
+//getting element by id and adding content to the html element
       document.getElementById("current-weather").innerHTML = html;
 
       return fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&appid=07d0c65f5c20674ff54bcddb4b9e892f&units=metric`);
@@ -33,7 +38,7 @@ submitButton.addEventListener('click', function (name) {
       console.log(data);
 
 
-
+//created a for loop for 5 days forecast from the fetch that returns 7 days forecast
       for (var i = 0; i < 5; i++) {
         console.log(data.timezone);
         console.log(data.daily[i].weather[0].icon);
@@ -55,7 +60,7 @@ submitButton.addEventListener('click', function (name) {
       <p id="uv-index"></p></div>`
         document.getElementById("5 Days Forecast").innerHTML += html;
       }
-      // data.daily has the 7 day forecast
+      
 
 
     });

2b2f88a953e1cbf961642b14813519fab2e5db9a
Sandeep GC <sandeep_gc@hotmail.com>
2021-05-07 13:20:12 +1000

    Adding 5 days forecast

--- script.js
+++ script.js
@@ -26,21 +26,40 @@ submitButton.addEventListener('click', function (name) {
 
       document.getElementById("current-weather").innerHTML = html;
 
-      return fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&appid=07d0c65f5c20674ff54bcddb4b9e892f`);
+      return fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&appid=07d0c65f5c20674ff54bcddb4b9e892f&units=metric`);
     })
     .then(response => response.json())
     .then(data => {
       console.log(data);
 
 
-      for(var i=0;i<5;i++){
-        console.log(data.daily[i].humidity)
+      for (var i = 0; i < 5; i++) {
+        console.log(data.timezone);
+        console.log(data.daily[i].weather[0].icon);
         console.log(data.daily[i].temp.day);
+        console.log(data.daily[i].humidity);
+        console.log(data.daily[i].wind_speed);
+
+        const html = `
+      <div class="card text-white bg-dark mb-3" style="max-width: 10rem;">
+      <div id="icon"><img src="https://openweathermap.org/img/w/${data.daily[i].weather[0].icon}.png"/></div>
+      <label>Location</label>
+      <p id="current-location">${data.timezone}</p>
+      <label>Temperature</label>
+      <p id="current-temp">${data.daily[i].temp.day}</p>
+      <label>Wind</label>
+      <p id="wind">${data.daily[i].wind_speed}</p>
+      <label>Humidity</label>
+      <p id="humidity">${data.daily[i].humidity}</p>
+      <label>UV Index</label>
+      <p id="uv-index"></p></div>`
+      document.getElementById("5 Days Forecast").innerHTML = html;
       }
       // data.daily has the 7 day forecast
-      
+
+
     });
-    
-  
+
+
 })
 
